1f2f4f13db18897691fc43df6d6006579161f0e9
Vani_3011 <vanitha@crayond.co>
2024-10-24 13:07:41 +0530

    To-do list

---
+++ screens/to-do-list/index.js
@@ -0,0 +1,127 @@
+import { useState } from "react"
+import { Alert, Dimensions, Modal, ScrollView, Text, TouchableOpacity, View } from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import MUIIcon from "react-native-vector-icons/MaterialIcons"
+
+import AddToDo from "./addTodo"
+import { styles } from "./style"
+import CheckBox from 'expo-checkbox'
+const ToDoList = () => {
+    const windowHeight = Dimensions.get('window').height;
+    const [text, setText] = useState({
+        name: ""
+    })
+    const [list, setList] = useState([
+        {
+            id: 1,
+            name: "Explore State management in react native using Todo app"
+        },
+        {
+            id: 2,
+            name: "Explore Navigations in react native"
+        }
+    ])
+    const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+
+    // Function to open the bottom sheet 
+    const handleOpenBottomSheet = () => {
+        setIsBottomSheetOpen(true);
+    };
+
+    // Function to close the bottom sheet
+    const handleCloseBottomSheet = () => {
+        setIsBottomSheetOpen(false);
+        setText({ name: "" })
+    };
+    const handleEdit = (val) => {
+        handleOpenBottomSheet()
+        setText({ ...val, is_edit: true })
+    }
+    const handleDelete = (val) => {
+        const result = list?.filter((x) => { return val?.id !== x?.id })
+        setList(result)
+        showToast("Deleted Successfully")
+    }
+    const showToast = (message) => {
+        Alert.alert(message)
+    }
+    const handleCheckbox = (val, data) => {
+        const result = list?.map((x) => {
+            return {
+                ...x,
+                isSelected: x?.id === data?.id ? val : x?.isSelected
+            }
+        })
+        setList(result)
+    }
+    const renderItem = (item) => (
+        <View style={item?.isSelected ? styles.listDisabledBox : styles.listBox} key={item?.id}>
+            <View style={styles.firstDiv}>
+                <CheckBox
+                    value={item?.isSelected}
+                    onValueChange={(val) => handleCheckbox(val, item)}
+                />
+                <Text style={[styles.listTitle, { textDecorationLine: item?.isSelected ? 'line-through' : 'none' }]}>{item?.name}</Text>
+            </View>
+            {!item?.isSelected &&
+                <View style={styles.editDiv}>
+                    <TouchableOpacity style={styles.trashIcon} onPress={() => item?.isSelected ? false : handleEdit(item)}>
+                        <MUIIcon name="edit" size={16} color={"blue"} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.trashIcon} onPress={() => item?.isSelected ? false : handleDelete(item)}>
+                        <Icon name="trash" size={16} color="red" />
+                    </TouchableOpacity>
+                </View>
+            }
+        </View>
+    );
+    return (
+        <View>
+            <View style={styles.root}>
+
+                <View style={styles.container}>
+                    <Text style={styles.title}>Todo List</Text>
+                </View>
+                <View style={styles.divider} />
+                {/* <ToastManager/> */}
+                <View style={styles.list}>
+                    {list?.length > 0 ?
+                        <ScrollView>
+                            {/* <FlatList
+                                data={list}
+                                renderItem={renderItem}
+                                keyExtractor={(item, index) => index.toString()}
+                                scrollEnabled={false}
+                                showsVerticalScrollIndicator={false}
+                                horizontal={false}
+                            /> */}
+                            {list?.map((x) => {
+                                return renderItem(x)
+                            })}
+
+                        </ScrollView>
+                        :
+                        <View style={styles.noData}>
+                            <Text style={styles.noDataText}>No List Found</Text>
+                        </View>
+                    }
+                </View>
+            </View>
+            <TouchableOpacity style={styles.fabButton} onPress={handleOpenBottomSheet}>
+                <Icon name="plus" size={20} color="#FFFFFF" />
+            </TouchableOpacity>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                // We use the state here to toggle visibility of Bottom Sheet 
+                visible={isBottomSheetOpen}
+                // We pass our function as default function to close the Modal
+                onRequestClose={handleCloseBottomSheet} >
+                <View style={[styles.bottomSheet, { height: windowHeight * 0.6 }]}>
+                    <AddToDo handleClose={handleCloseBottomSheet} list={list} setList={setList} text={text} setText={setText} showToast={showToast} />
+                </View>
+            </Modal>
+        </View>
+    )
+}
+export default ToDoList
---
+++ screens/to-do-list/style.js
@@ -0,0 +1,136 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    root:{
+        position:"relative",
+    },
+    container: {
+        // flex: 1,
+        // backgroundImage:'linear-gradient(to right top, #f6f2ef, #f1eeec, #ece9e8, #e7e5e5, #e2e1e1, #e2ddde, #e1d9dd, #ddd5de, #e4cfdb, #edc9d2, #f4c4c4, #f6c1b2)',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        padding: 12,
+        backgroundColor:"#FFFFFF"
+    },
+    addcontainer: {
+        // flex: 1,
+        // backgroundImage:'linear-gradient(to right top, #f6f2ef, #f1eeec, #ece9e8, #e7e5e5, #e2e1e1, #e2ddde, #e1d9dd, #ddd5de, #e4cfdb, #edc9d2, #f4c4c4, #f6c1b2)',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection:"row",
+        margin: 12,
+        backgroundColor:"#FFFFFF"
+    },
+    title: {
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 16,
+        textTransform: "uppercase",
+        color: "#091B29",
+        letterSpacing: 0.5,
+    },
+    addtitle:{
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 14,
+        textTransform: "capitalize",
+        color: "#091B29",
+        letterSpacing: 0.5,
+    },
+    divider: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#E4E8EE"
+    },
+    list:{
+        padding:12,
+    },
+    listBox:{
+        marginBottom:8,
+        backgroundColor:"#FFFFFF",
+        padding:12,
+        borderRadius:8,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    listDisabledBox:{
+        marginBottom:8,
+        backgroundColor:"#E4E8EE",
+        padding:12,
+        borderRadius:8,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    listTitle:{
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 14,
+        maxWidth:220,
+        color:"#4E5A6B",
+        paddingLeft:8
+    },
+    editDiv:{
+        flexDirection: "row",
+        // justifyContent: "space-between",
+        // alignItems: "center",
+    },
+    firstDiv:{
+        flexDirection: "row",
+        alignItems:'center'
+    },
+    textBoxContainer:{
+        padding:12,
+    },
+    trashIcon:{
+        padding:6
+    },
+    fabButton:{
+        borderRadius:50,
+        width:40,
+        height:40,
+        // flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#5C5CFF",
+        position:"absolute",
+        top:480,
+        bottom:0,
+        right:0,
+        margin:16
+    },
+    bottomSheet: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        // justifyContent: 'flex-start',
+        // alignItems: 'center',
+        backgroundColor: 'white',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        // paddingVertical: 23,
+        // paddingHorizontal: 25,
+        bottom: 0,
+        borderWidth: 1,
+        borderColor: '#E4E8EE'
+    },
+    customTextInput:{
+        height: 50,
+        borderWidth:1,
+        borderColor:"#E4E8EE",
+        borderRadius:4,
+        padding:12 
+    },
+    customButton:{
+        borderRadius:4,
+    },
+    noData:{
+        flexDirection:"row",
+        justifyContent:"center"
+    },
+    noDataText:{
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 14,
+        maxWidth:250,
+        color:"#4E5A6B",
+        top:150,
+        bottom:150
+    }
+})
